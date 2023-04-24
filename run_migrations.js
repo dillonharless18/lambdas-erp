59e2c8ce17da8fe6b430e0239f65b38df7b542e8
dillonharless18 <dillonharless@gmail.com>
2023-04-24 09:52:58 -0400

    updated run_migrations to match the changes to the CodeBuildStep in CDK

--- run_migrations.js
+++ run_migrations.js
@@ -1,9 +1,16 @@
 const Knex = require('knex');
 const config = require('./knexfile');
 
+// Retrieve the database credentials from the environment variable
+const databaseCredentials = JSON.parse(process.env.DB_CREDENTIALS);
+
 // Set the environment (you can set it based on a NODE_ENV variable or use a specific config)
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = config[environment];
+
+// Set the user and password in the knex config
+knexConfig.connection.user = databaseCredentials.user;
+knexConfig.connection.password = databaseCredentials.password;
 
 // Initialize the Knex instance
 const knex = Knex(knexConfig);
