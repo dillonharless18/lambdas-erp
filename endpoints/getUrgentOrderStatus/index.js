93afa9cf7b4f79732154c2f7c0438572b6602596
navaid7890 <navaidrasheed221@gmail.com>
2023-05-04 23:41:02 +0500

    secret manager implementation

--- endpoints/getUrgentOrderStatus/index.js
+++ endpoints/getUrgentOrderStatus/index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const db = require('./db');
 
-app.get('', async (req, res) => {
+app.get('/urgentOrderStatus', async (req, res) => {
   try {
-    const urgentOrderStatus = await db('onexerp').select('*').from('urgent_order_status');
-    res.json(urgentOrderStatus);
+    const urgetOrderStatus = await db('onexerp').select('*').from('urgent_order_status');
+    res.json(urgetOrderStatus);
   } catch (error) {
     console.error('Error fetching urgetOrderStatus:', error);
     res.status(500).json({ error: 'Internal Server Error' });
--- endpoints/getUrgentOrderStatus/knexfile.js
+++ endpoints/getUrgentOrderStatus/knexfile.js
@@ -1,12 +1,30 @@
-require('dotenv').config();
+const AWS = require('aws-sdk');
 
-module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.PORT,
+const getSecrets = async () => {
+  const secretsManager = new AWS.SecretsManager({
+    region: 'us-east-1', // Replace 'your-region' with your AWS region
+  });
+
+  const secretId = 'arn:aws:secretsmanager:us-east-1:136559125535:secret:database-credentials-GeMfwW'; // Replace 'your-secret-id' with your AWS Secret Manager secret ID
+
+  try {
+    const response = await secretsManager.getSecretValue({ SecretId: secretId }).promise();
+    const secrets = JSON.parse(response.SecretString);
+
+    return {
+      client: 'pg',
+      connection: {
+        host: secrets.host,
+        user: secrets.username,
+        password: secrets.password,
+        database: secrets.dbname,
+        port: secrets.port,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching secrets:', error);
+    throw error;
   }
 };
+
+module.exports = getSecrets();
