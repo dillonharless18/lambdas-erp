8f1e868753e9f217ca8b4ea4f7b9200b937bc398
navaid7890 <navaidrasheed221@gmail.com>
2023-05-09 00:07:27 +0500

    removed test lambda

---
+++ endpoints/getAllProjects/index.js
@@ -0,0 +1,13 @@
+const { getAllProjects } = require("./getAllProjects");
+
+exports.handler = async () => {
+  try {
+    return await getAllProjects();
+  } catch (error) {
+    console.error("Error in handler:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Server Error, ${error}` }),
+    };
+  }
+};
