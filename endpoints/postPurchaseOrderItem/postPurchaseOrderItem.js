884967506bcd6c1ad116349bb4040377a7c5d889
navaid7890 <navaidrasheed221@gmail.com>
2023-05-15 13:32:29 +0500

    postPurchaseOrderItem error handling on item array

--- endpoints/postPurchaseOrderItem/postPurchaseOrderItem.js
+++ endpoints/postPurchaseOrderItem/postPurchaseOrderItem.js
@@ -18,25 +18,30 @@ const initializeDb = async () => {
 
 module.exports.postPurchaseOrderItems = async (items) => {
   await initializeDb();
+
+  if (!Array.isArray(items)) {
+    throw new Error("The items parameter must be an array");
+  }
+
+  const purchaseOrderItems = items.map((item) => new PurchaseOrderItem(item));
+
+  const dataToInsert = purchaseOrderItems.map((item) => ({
+    purchase_order_request_id: item.purchase_order_request_id,
+    quantity: item.quantity,
+    item_name: item.item_name,
+    unit_of_measurement: item.unit_of_measurement,
+    suggested_vendor: item.suggested_vendor,
+    urgent_order_status: item.urgent_order_status,
+    project_id: item.project_id,
+    description: item.description,
+    s3_uri: item.s3_uri,
+    user_id: item.user_id,
+    created_at: item.created_at,
+    purchase_order_request_item_status_id:
+      item.purchase_order_request_item_status_id,
+  }));
+
   try {
-    const purchaseOrderItems = items.map((item) => new PurchaseOrderItem(item));
-
-    const dataToInsert = purchaseOrderItems.map((item) => ({
-      purchase_order_request_id: item.purchase_order_request_id,
-      quantity: item.quantity,
-      item_name: item.item_name,
-      unit_of_measurement: item.unit_of_measurement,
-      suggested_vendor: item.suggested_vendor,
-      urgent_order_status: item.urgent_order_status,
-      project_id: item.project_id,
-      description: item.description,
-      s3_uri: item.s3_uri,
-      user_id: item.user_id,
-      created_at: item.created_at,
-      purchase_order_request_item_status_id:
-        item.purchase_order_request_item_status_id,
-    }));
-
     await knexInstance("purchase_order_items").insert(dataToInsert);
 
     return {
