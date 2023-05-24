1f6ded24cc3ef112dee00da603653f08c435acdf
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-05-24 10:39:48 +0500

    getPurchaseOrderRequestItems query changed and included header.

--- endpoints/getPurchaseOrderRequestItems/getPurchaseOrderRequestItems.js
+++ endpoints/getPurchaseOrderRequestItems/getPurchaseOrderRequestItems.js
@@ -3,14 +3,14 @@ import initializeKnex from "/opt/nodejs/db/index.js";
 let knexInstance;
 
 const initializeDb = async () => {
-  try {
-    if (!knexInstance) {
-      knexInstance = await initializeKnex();
+    try {
+        if (!knexInstance) {
+            knexInstance = await initializeKnex();
+        }
+    } catch (error) {
+        console.error("Error initializing database:", error);
+        throw error;
     }
-  } catch (error) {
-    console.error("Error initializing database:", error);
-    throw error;
-  }
 };
 
 const getPurchaseOrderRequestItems = async (status) => {
@@ -19,74 +19,69 @@ const getPurchaseOrderRequestItems = async (status) => {
     const getAllPurchaseOrderRequestItems = await knexInstance(
       "purchase_order_request_item"
     )
-      .select(
-        "purchase_order_request_item.purchase_order_request_item_id AS purchase_order_request_id",
-        "purchase_order_request_item.quantity",
-        "purchase_order_request_item.item_name",
-        "purchase_order_request_item.unit_of_measure AS unit_of_measurement",
-        "purchase_order_request_item.suggested_vendor",
-        "purchase_order_request_item.description",
-        "purchase_order_request_item.s3_uri",
-        "purchase_order_request_item.created_at as requestDate",
-        knexInstance.raw(`json_agg(
-    json_build_object(
-      'urgent_order_status_id', urgent_order_status.urgent_order_status_id,
-      'urgent_order_status_name', urgent_order_status.urgent_order_status_name
-    )
-  ) AS urgent_order_status`),
-        knexInstance.raw(`json_agg(
-    json_build_object(
-      'project_id', project.project_id,
-      'project_name', project.project_name
-    )
-  ) AS project`),
-        knexInstance.raw(`json_agg(
-    json_build_object(
-      'vendor_id', vendor.vendor_id,
-      'vendor_name', vendor.vendor_name
-    )
-  ) AS vendor`),
-        knexInstance.raw(`json_agg(
-    json_build_object(
-      'requester', user.user_id,
-      'requesterFirstName', user.first_name,
-      'requesterLastName, user.last_name
-    )
-  ) AS requester`)
+      .join(
+        "user as createdBy",
+        "createdBy.user_id",
+        "=",
+        "purchase_order_request_item.created_by"
       )
-      .leftJoin(
+      .join(
+        "user as updatedBy",
+        "updatedBy.user_id",
+        "=",
+        "purchase_order_request_item.last_updated_by"
+      )
+      .join(
         "project",
-        "purchase_order_request_item.project_id",
+        "project.project_id",
         "=",
-        "project.project_id"
+        "purchase_order_request_item.project_id"
       )
-      .leftJoin(
+      .join(
         "urgent_order_status",
-        "purchase_order_request_item.urgent_order_status_id",
+        "urgent_order_status.urgent_order_status_id",
         "=",
-        "urgent_order_status.urgent_order_status_id"
+        "purchase_order_request_item.urgent_order_status_id"
       )
-      .leftJoin(
+      .join(
         "vendor",
-        "purchase_order_request_item.vendor_id",
+        "vendor.vendor_id",
         "=",
-        "vendor.vendor_id"
+        "purchase_order_request_item.vendor_id"
       )
-      .leftJoin(
-        "requester",
-        "purchase_order_request_item.created_by",
-        "=",
-        "user.user_id"
-      )
-      .leftJoin(
+      .join(
         "purchase_order_request_item_status",
-        "purchase_order_request_item.purchase_order_request_item_status_id",
+        "purchase_order_request_item_status.purchase_order_request_item_status_id",
         "=",
-        "purchase_order_request_item_status.purchase_order_request_item_status_id"
+        "purchase_order_request_item.purchase_order_request_item_status_id"
       )
-      .groupBy("purchase_order_request_item.purchase_order_request_item_id")
+      .select(
+        "purchase_order_request_item.purchase_order_request_item_id",
+        "purchase_order_request_item.last_updated_by",
+        "purchase_order_request_item.created_by",
+        "purchase_order_request_item.item_name",
+        "purchase_order_request_item.quantity",
+        "purchase_order_request_item.unit_of_measure",
+        "purchase_order_request_item.suggested_vendor",
+        "purchase_order_request_item.s3_uri",
+        "purchase_order_request_item.description",
+        "purchase_order_request_item.created_at as requestedDate",
+        "purchase_order_request_item.last_updated_at",
+        "purchase_order_request_item.price",
+        "purchase_order_request_item.vendor_id",
+        "purchase_order_request_item.project_id",
+        "purchase_order_request_item.purchase_order_request_item_status_id",
+        "purchase_order_request_item.urgent_order_status_id",
+        "createdBy.first_name as requester",
+        "updatedBy.first_name as updatedBy",
+        "project.project_name",
+        "urgent_order_status.urgent_order_status_name as urgent_status",
+        "vendor.vendor_name",
+        "purchase_order_request_item_status.purchase_order_request_item_status_name"
+      )
       .where(
         "purchase_order_request_item_status.purchase_order_request_item_status_name",
+        "=",
         status
       );
 
@@ -98,7 +93,9 @@ const getPurchaseOrderRequestItems = async (status) => {
     console.error("Error fetching PurchaseOrderRequestItems:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: `Server Error, ${error}` }),
+      body: JSON.stringify({
+        error: `Server Error, ${error}`,
+      }),
     };
   }
 };
