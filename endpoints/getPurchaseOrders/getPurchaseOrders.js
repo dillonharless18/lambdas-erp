993300f74ee50b61e5730720a67c9fca58df3419
Shakir Ullah <shakir@cloudfruit.com>
2023-06-01 21:50:26 +0500

    get purchase order items along purchase order

--- endpoints/getPurchaseOrders/getPurchaseOrders.js
+++ endpoints/getPurchaseOrders/getPurchaseOrders.js
@@ -17,6 +17,24 @@ const getPurchaseOrders = async (status) => {
     await initializeDb();
     try {
         const getPurchaseOrders = await knexInstance("purchase_order")
+            .select(
+                "purchase_order.purchase_order_id",
+                "purchase_order.last_updated_by",
+                "purchase_order.created_by",
+                "purchase_order.created_at",
+                "purchase_order.total_price",
+                "purchase_order.purchase_order_number",
+                "purchase_order.vendor_id",
+                "purchase_order.purchase_order_status_id",
+                "purchase_order.quickbooks_purchase_order_id",
+                "purchase_order.s3_uri",
+                knexInstance.raw(
+                    '("createdBy".first_name || \' \' || "createdBy".last_name) AS requester'
+                ),
+                "vendor.vendor_name",
+                "purchase_order_status.purchase_order_status_name",
+                knexInstance.raw('JSON_AGG(purchase_order_item.*) as purchase_order_items')
+            )
             .join(
                 "user as createdBy",
                 "createdBy.user_id",
@@ -30,29 +48,26 @@ const getPurchaseOrders = async (status) => {
                 "=",
                 "purchase_order.purchase_order_status_id"
             )
-            .select(
-                "purchase_order.purchase_order_id",
+            .join('purchase_order_item', 'purchase_order.purchase_order_id', 'purchase_order_item.purchase_order_id')
+            .where(
+                "purchase_order_status.purchase_order_status_name",
+                "=",
+                status
+            )
+            .groupBy("purchase_order.purchase_order_id",
                 "purchase_order.last_updated_by",
                 "purchase_order.created_by",
                 "purchase_order.created_at",
-                "purchase_order.created_by",
                 "purchase_order.total_price",
                 "purchase_order.purchase_order_number",
                 "purchase_order.vendor_id",
                 "purchase_order.purchase_order_status_id",
                 "purchase_order.quickbooks_purchase_order_id",
                 "purchase_order.s3_uri",
-                knexInstance.raw(
-                    '("createdBy".first_name || \' \' || "createdBy".last_name) AS requester'
-                ),
+                "requester",
                 "vendor.vendor_name",
                 "purchase_order_status.purchase_order_status_name"
             )
-            .where(
-                "purchase_order_status.purchase_order_status_name",
-                "=",
-                status
-            );
 
         return {
             statusCode: 200,
