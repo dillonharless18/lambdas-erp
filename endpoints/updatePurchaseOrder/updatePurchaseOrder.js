fe40c03cdfd79183ca531253a2f09b35fb5a7699
Shakir Ullah <shakir@cloudfruit.com>
2023-06-08 19:30:06 +0500

    updated

--- endpoints/updatePurchaseOrder/updatePurchaseOrder.js
+++ endpoints/updatePurchaseOrder/updatePurchaseOrder.js
@@ -21,15 +21,15 @@ const updatePurchaseOrder = async (purchaseOrder) => {
     throw new Error('Missing purchase_order_id');
   }
 
-  let purchaseOrder = new PurchaseOrder(item);
+  let purchaseOrderData = new PurchaseOrder(purchaseOrder);
 
-  purchaseOrder = Object.fromEntries(
-    Object.entries(purchaseOrder).filter(([_, val]) => val)
+  purchaseOrderData = Object.fromEntries(
+    Object.entries(purchaseOrderData).filter(([_, val]) => val)
   ); // remove null or empty values
 
   await knexInstance('purchase_order')
-    .where('purchase_order_id', purchaseOrder.purchase_order_id)
-    .update(purchaseOrder);
+    .where('purchase_order_id', purchaseOrderData.purchase_order_id)
+    .update(purchaseOrderData);
 
   return {
     statusCode: 200,
