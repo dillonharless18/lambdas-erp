401b3c7d6c06059bd90f8ba68a390f4b5ddd87b0
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-06-14 11:10:45 +0500

    changed metadata.json for ocrImportedPurchaseOrder

--- endpoints/postOcrImportedPurchaseOrder/index.js
+++ endpoints/postOcrImportedPurchaseOrder/index.js
@@ -2,9 +2,23 @@ import postOcrImportedPurchaseOrder from './postOcrImportedPurchaseOrder.js';
 
 const handler = async (event) => {
   try {
+    const ocrImportedPurchaseOrderId =
+      event.pathParameters?.ocrImportedPurchaseOrderId;
     const body = JSON.parse(event.body).ocrImportedPurchaseOrder;
 
-    return await postOcrImportedPurchaseOrder(body);
+    if (!ocrImportedPurchaseOrderId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error: 'Missing ocrImportedPurchaseOrderId path parameter',
+        }),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      };
+    }
+
+    return await postOcrImportedPurchaseOrder(ocrImportedPurchaseOrderId, body);
   } catch (error) {
     console.error('Error in handler:', error);
     return {
