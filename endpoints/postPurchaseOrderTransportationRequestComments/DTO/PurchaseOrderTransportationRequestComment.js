4d6d8803eaf5190fd229962d172a7c8bd384dc47
dillonharless18 <dillonharless@gmail.com>
2023-06-19 13:25:03 -0400

    changing POST request metadata to singular from plural for transportation requests comments

--- endpoints/postPurchaseOrderTransportationRequestComments/DTO/PurchaseOrderTransportationRequestComment.js
+++
@@ -1,7 +0,0 @@
-class PurchaseOrderTransportationRequestComment {
-  constructor(comment) {
-    this.comment_text = item.comment_text;
-  }
-}
-
-export default PurchaseOrderTransportationRequestComment;
