cf50f039d147ab025ae1f162b1c6ba89aec91f89
Shakir Ullah <shakir@cloudfruit.com>
2023-06-22 19:49:57 +0500

    updated postTransportationTrip API to handle multiple purchaseOrderTransportationRequests

--- endpoints/postTransportationTrip/DTO/TransportationTrip.js
+++ endpoints/postTransportationTrip/DTO/TransportationTrip.js
@@ -3,6 +3,7 @@ class TransportationTrip {
     this.driver_id = trip.driver_id;
     this.trip_name = trip.trip_name;
     this.vehicle_type_id = trip.vehicle_type_id;
+    this.purchase_order_transportation_request_ids = trip.purchase_order_transportation_request_id
   }
 }
 
