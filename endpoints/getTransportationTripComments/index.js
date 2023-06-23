50abf122cf212c6788a6210e5c30bf595db6e81d
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-06-23 11:00:33 +0500

    getTransportationTripComments

---
+++ endpoints/getTransportationTripComments/index.js
@@ -0,0 +1,31 @@
+import getTransportationTripComments from './getTransportationTripComments.js';
+
+const handler = async (event, context) => {
+  try {
+    const transportationTripId = event.pathParameters?.transportaion_trip_id;
+    if (!transportationTripId) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          error:
+            'Missing purchase_order_transportation_request_id path parameter',
+        }),
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      };
+    }
+    return await getTransportationTripComments(transportationTripId);
+  } catch (error) {
+    console.error('Error in handler:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: `Server Error, ${error}` }),
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+  }
+};
+
+export { handler };
