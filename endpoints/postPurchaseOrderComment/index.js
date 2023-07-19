74ef33f57e170660da2b63c7c1850aaa33f0ceaa
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-07-19 15:27:55 +0500

    authentication work on postPurchaseOrderComment

--- endpoints/postPurchaseOrderComment/index.js
+++ endpoints/postPurchaseOrderComment/index.js
@@ -4,8 +4,9 @@ const handler = async (event) => {
   try {
     const comment = JSON.parse(event.body).comment;
     const purchaseOrderId = event.pathParameters?.purchase_order_id;
+    const userSub = event.requestContext.authorizer.sub;
 
-    return await postPurchaseOrderComment(comment, purchaseOrderId);
+    return await postPurchaseOrderComment(comment, purchaseOrderId, userSub);
   } catch (error) {
     console.error('Error in handler:', error);
     return {
