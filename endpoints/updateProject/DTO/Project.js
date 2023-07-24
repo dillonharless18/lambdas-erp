683606be3678a94b5ce3e83f77d5f901d774a5d3
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-07-24 12:58:38 +0500

    updateProject lambda

---
+++ endpoints/updateProject/DTO/Project.js
@@ -0,0 +1,10 @@
+class Project {
+  constructor(project) {
+    this.project_name = project.project_name;
+    this.project_code = project.project_code;
+    this.material_budget = project.material_budget;
+    this.labor_budget = project.labor_budget;
+  }
+}
+
+export default Project;
