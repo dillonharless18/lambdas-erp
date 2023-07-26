adf8cc41e5ff0af34a820dba04f1bf7bf029908a
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-07-26 11:12:57 +0500

    updated updateProject query

--- endpoints/updateProject/index.js
+++ endpoints/updateProject/index.js
@@ -2,10 +2,11 @@ import updateProject from './updateProject.js';
 
 const handler = async (event) => {
   try {
+    const projectId = event.pathParameters?.project_id;
     const body = JSON.parse(event.body).project;
     const userSub = event.requestContext.authorizer.sub;
 
-    return await updateProject(body, userSub);
+    return await updateProject(projectId, body, userSub);
   } catch (error) {
     console.error('Error in handler:', error);
     return {
