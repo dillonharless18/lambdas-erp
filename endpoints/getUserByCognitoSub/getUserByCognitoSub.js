64e2ce32b556175401db12fd975e04d893b22a79
chaa-cf <125417508+chaa-cf@users.noreply.github.com>
2023-08-25 09:32:22 -0400

    Update endpoints/getUserByCognitoSub/getUserByCognitoSub.js

--- endpoints/getUserByCognitoSub/getUserByCognitoSub.js
+++ endpoints/getUserByCognitoSub/getUserByCognitoSub.js
@@ -10,7 +10,6 @@ const initializeDb = async () => {
       knexInstance = await initializeKnex();
     }
   } catch (error) {
-    // console.error('Error initializing database:', error);
     throw new DatabaseError('Failed to initialize the database.');
   }
 };
