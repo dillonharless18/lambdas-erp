41ba8b14da5e025c52a3730cd799c864687c6cd7
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-08-30 16:20:10 +0400

    getAllVendors included, createdBy and updatedBy columns

--- endpoints/getAllVendors/getAllVendors.js
+++ endpoints/getAllVendors/getAllVendors.js
@@ -16,10 +16,29 @@ const initializeDb = async () => {
 const getAllVendors = async () => {
   await initializeDb();
   try {
-    const AllVendors = await knexInstance.select('*').from('vendor');
+    // const AllVendors = await knexInstance.select('*').from('vendor');
+    const allVendors = await knexInstance
+      .select(
+        'v.*',
+        knexInstance.raw(
+          '("createdBy".first_name || \' \' || "createdBy".last_name) AS CreatedBy'
+        ),
+        knexInstance.raw(
+          '("updatedBy".first_name || \' \' || "updatedBy".last_name) AS UpdatedBy'
+        )
+      )
+      .from('vendor as v')
+      .join('user as createdBy', 'createdBy.created_by', '=', 'v.created_by')
+      .join(
+        'user as updatedBy',
+        'updatedBy.last_updated_by',
+        '=',
+        'v.last_updated_by'
+      )
+      .where('v.is_active', true);
     return {
       statusCode: 200,
-      body: JSON.stringify(AllVendors),
+      body: JSON.stringify(allVendors),
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
