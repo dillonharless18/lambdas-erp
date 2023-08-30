446d6ac8f48d62e2bd36da4fce217041dc956cf9
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-08-30 17:28:41 +0400

    getAllProjects included, createdBy and updatedBy columns

--- endpoints/getAllProjects/getAllProjects.js
+++ endpoints/getAllProjects/getAllProjects.js
@@ -18,7 +18,26 @@ const initializeDb = async () => {
 const getAllProjects = async () => {
   await initializeDb();
   try {
-    const projects = await knexInstance.select('*').from('project');
+    // const projects = await knexInstance.select('*').from('project');
+    const projects = await knexInstance
+      .select(
+        'p.*',
+        knexInstance.raw(
+          '("createdBy".first_name || \' \' || "createdBy".last_name) AS CreatedBy'
+        ),
+        knexInstance.raw(
+          '("updatedBy".first_name || \' \' || "updatedBy".last_name) AS UpdatedBy'
+        )
+      )
+      .from('project as p')
+      .join('user as createdBy', 'createdBy.created_by', '=', 'p.created_by')
+      .join(
+        'user as updatedBy',
+        'updatedBy.last_updated_by',
+        '=',
+        'p.last_updated_by'
+      )
+      .where('p.is_active', true);
 
     if (!projects || projects.length === 0) {
       throw new NotFoundError('No projects found.');
