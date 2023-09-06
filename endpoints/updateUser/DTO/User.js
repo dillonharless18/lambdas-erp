04cdde5ecb0f8ffe3d0ab9691d1adcb9d49d00a7
Chaa Loftin <chaamail@yahoo.com>
2023-09-05 20:55:54 -0400

    added phone number formatting to user phone number

--- endpoints/updateUser/DTO/User.js
+++ endpoints/updateUser/DTO/User.js
@@ -1,13 +1,13 @@
 class User {
-  constructor(user) {
-    this.first_name = user.first_name;
-    this.last_name = user.last_name;
-    this.phone_number = user.phone_number;
-    this.user_email = user.user_email;
-    this.user_role = user.user_role;
-    this.ocr_tool_id = user.ocr_tool_id;
-    this.is_active = user.is_active
-  }
+    constructor(user) {
+        this.first_name = user.first_name;
+        this.last_name = user.last_name;
+        this.phone_number = user.phone_number.replace(/[\s()-]/g, "");
+        this.user_email = user.user_email;
+        this.user_role = user.user_role;
+        this.ocr_tool_id = user.ocr_tool_id;
+        this.is_active = user.is_active;
+    }
 }
 
 export default User;
