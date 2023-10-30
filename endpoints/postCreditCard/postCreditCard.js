06602584abb81ab2f26436429bcd191826b16145
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-10-30 17:44:39 +0500

    custom error handling on postCreditCard

--- endpoints/postCreditCard/postCreditCard.js
+++ endpoints/postCreditCard/postCreditCard.js
@@ -1,5 +1,11 @@
 import CreditCard from './DTO/CreditCard.js';
 import initializeKnex from '/opt/nodejs/db/index.js';
+import {
+  BadRequestError,
+  InternalServerError,
+  DatabaseError,
+} from '/opt/nodejs/errors.js';
+import { createSuccessResponse } from '/opt/nodejs/apiResponseUtil.js';
 
 let knexInstance;
 
@@ -9,51 +15,46 @@ const initializeDb = async () => {
       knexInstance = await initializeKnex();
     }
   } catch (error) {
-    console.error('Error initializing database:', error);
-    throw error;
+    console.error('Error initializing database:', error.stack);
+    throw new DatabaseError('Failed to initialize the database.');
   }
 };
 
 const postCreditCard = async (creaditCardData, userSub) => {
   await initializeDb();
 
-  if (typeof creaditCardData !== 'object' || creaditCardData === null) {
-    console.error('Error: The creaditCard parameter must be an object');
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        error:
-          'Invalid input format: The creaditCard parameter must be an object',
-      }),
+  try {
+    if (typeof creaditCardData !== 'object' || creaditCardData === null) {
+      console.error('Error: The creaditCard parameter must be an object');
+      throw new BadRequestError(
+        'Invalid input format: The creaditCard parameter must be an object'
+      );
+    }
+
+    const user = await knexInstance('user')
+      .where('cognito_sub', userSub)
+      .pluck('user_id');
+
+    const creditCard = new CreditCard(creaditCardData);
+
+    let dataToInsert = {
+      last_updated_by: user[0],
+      last_updated_at: knexInstance.raw('NOW()'),
+      created_by: user[0],
+      created_at: knexInstance.raw('NOW()'),
+      is_active: true,
+      ...creditCard,
     };
-  }
 
-  const user = await knexInstance('user')
-    .where('cognito_sub', userSub)
-    .pluck('user_id');
+    await knexInstance('credit_card').insert(dataToInsert);
 
-  const creditCard = new CreditCard(creaditCardData);
-
-  let dataToInsert = {
-    last_updated_by: user[0],
-    last_updated_at: knexInstance.raw('NOW()'),
-    created_by: user[0],
-    created_at: knexInstance.raw('NOW()'),
-    is_active: true,
-    ...creditCard,
-  };
-
-  await knexInstance('credit_card').insert(dataToInsert);
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
+    return createSuccessResponse({
       message: 'Credit Card added successfully!',
-    }),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  };
+    });
+  } catch (error) {
+    console.error(error.stack);
+    throw new InternalServerError();
+  }
 };
 
 export default postCreditCard;
