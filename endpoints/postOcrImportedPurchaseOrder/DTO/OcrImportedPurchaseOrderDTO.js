d9013da40940672b529f9a01e4a2ffbc7ea59e44
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-10-31 18:37:24 +0500

    included tax columns in PO and OCR PO post endpoints

--- endpoints/postOcrImportedPurchaseOrder/DTO/OcrImportedPurchaseOrderDTO.js
+++ endpoints/postOcrImportedPurchaseOrder/DTO/OcrImportedPurchaseOrderDTO.js
@@ -7,6 +7,8 @@ class OcrImportedPurchaseOrderDTO {
     this.total_price = purchaseOrder.total_price;
     this.vendor_id = purchaseOrder.vendor_id;
     this.s3_uri = purchaseOrder.s3_uri;
+    this.shipping_cost = purchaseOrder.shipping_cost;
+    this.estimated_taxes = purchaseOrder.estimated_taxes;
     this.purchaseOrderItems =
       purchaseOrder.ocr_imported_purchase_order_items.map(
         (item) => new OcrImportedPurchaseOrderItemDTO(item)
