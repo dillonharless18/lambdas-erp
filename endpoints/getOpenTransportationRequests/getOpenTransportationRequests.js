19dd23e842bc6887a610e49bbda2183c5425fa62
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-11-06 16:19:08 +0500

    include po items and vendor details

--- endpoints/getOpenTransportationRequests/getOpenTransportationRequests.js
+++ endpoints/getOpenTransportationRequests/getOpenTransportationRequests.js
@@ -26,6 +26,8 @@ const getOpenTransportationRequests = async () => {
         'p.project_name',
         knexInstance.raw("CONCAT(u.first_name, ' ', u.last_name) as requester"),
         'po.purchase_order_number',
+        'po.vendor_id',
+        'v.vendor_name as vendorName',
         'trs.transportation_request_status_name',
         'trt.transportation_request_type_name',
         'uos.urgent_order_status_name',
@@ -33,6 +35,11 @@ const getOpenTransportationRequests = async () => {
           'COALESCE(comments.comment_count::INTEGER, 0) as comment_count'
         ),
       ])
+      .select(
+        knexInstance.raw(
+          "json_agg(json_build_object('purchaseOrderItemId' , pot.purchase_order_item_id, 'unitOfMeasure', pot.unit_of_measure, 'description', pot.description ,'price', pot.price, 'quantity', pot.quantity, 'isDamaged', pot.is_damaged, 'damageOrReturnText', pot.damage_or_return_text, 'projectId', pot.project_id, 'purchaseOrderItemStatusId', pot.purchase_order_item_status_id, 's3Uri', pot.s3_uri, 'itemName', pot.item_name, 'suggestedVendor', pot.suggested_vendor, 'urgentOrderStatusId', pot.urgent_order_status_id)) as purchase_order_items"
+        )
+      )
       .leftJoin(
         knexInstance('purchase_order_transportation_request_comment')
           .select('purchase_order_transportation_request_id')
@@ -50,6 +57,7 @@ const getOpenTransportationRequests = async () => {
         'po.purchase_order_id',
         'potr.purchase_order_id'
       )
+      .leftJoin('vendor as v', 'v.vendor_id', 'po.vendor_id')
       .leftJoin(
         'transportation_request_status as trs',
         'trs.transportation_request_status_id',
@@ -64,6 +72,37 @@ const getOpenTransportationRequests = async () => {
         'urgent_order_status as uos',
         'uos.urgent_order_status_id',
         'potr.urgent_order_status_id'
+      )
+      .leftJoin(
+        'purchase_order_item as pot',
+        'pot.purchase_order_id',
+        'po.purchase_order_id'
+      )
+      .groupBy(
+        'potr.purchase_order_transportation_request_id',
+        'potr.purchase_order_id',
+        'potr.transportation_request_type_id',
+        'potr.from_location',
+        'potr.to_location',
+        'potr.additional_details',
+        'potr.urgent_order_status_id',
+        'potr.transportation_request_status_id',
+        'potr.future_transportation_date',
+        'potr.item_name',
+        'potr.recipients',
+        'potr.contact_number',
+        'potr.contact_name',
+        'potr.project_id',
+        'potr.is_active',
+        'p.project_name',
+        'requester',
+        'po.purchase_order_number',
+        'trs.transportation_request_status_name',
+        'trt.transportation_request_type_name',
+        'uos.urgent_order_status_name',
+        'comment_count',
+        'vendorName',
+        'po.vendor_id'
       )
       .where('potr.transportation_request_status_id', 1)
       .andWhere('potr.is_active', true)
