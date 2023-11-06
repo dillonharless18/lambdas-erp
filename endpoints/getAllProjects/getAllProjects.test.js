c05439efc9c2d0005cbd741b53a5aa11d0686d22
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-11-06 18:30:01 +0500

    included 2 more tests

--- endpoints/getAllProjects/getAllProjects.test.js
+++ endpoints/getAllProjects/getAllProjects.test.js
@@ -1,7 +1,6 @@
 // Import mock-knex
 const mockDb = require('mock-knex');
 
-// Mock the Lambda layer paths
 jest.mock(
   '/opt/nodejs/db/index.js',
   () => ({
@@ -9,7 +8,6 @@ jest.mock(
     default: () => {
       const knex = require('knex')({
         client: 'pg',
-        // connection details are not needed as we are mocking the db
       });
       mockDb.mock(knex);
       return knex;
@@ -39,6 +37,8 @@ jest.mock(
   }),
   { virtual: true }
 );
+
+const errors = jest.requireMock('/opt/nodejs/errors.js');
 
 jest.mock(
   '/opt/nodejs/apiResponseUtil.js',
@@ -71,10 +71,8 @@ describe('getAllProjects', () => {
     // Clear all mocks before each test
     jest.clearAllMocks();
 
-    // Import the getAllProjects function after the mocks are set up
     getAllProjects = require('./getAllProjects').default;
 
-    // Tracker to mock the responses
     const tracker = mockDb.getTracker();
     tracker.install();
     tracker.on('query', (query) => {
@@ -98,7 +96,6 @@ describe('getAllProjects', () => {
   });
 
   afterEach(() => {
-    // Uninstall the tracker after each test
     mockDb.getTracker().uninstall();
   });
 
@@ -119,5 +116,66 @@ describe('getAllProjects', () => {
 
     const body = JSON.parse(result.body);
     expect(body).toBeInstanceOf(Array);
+  });
+
+  it('should throw NotFoundError when no projects are found', async () => {
+    const event = {
+      queryStringParameters: {
+        isAll: 'false',
+      },
+    };
+
+    const tracker = mockDb.getTracker();
+    tracker.install();
+    tracker.on('query', (query, step) => {
+      if (step === 1) {
+        query.response([]);
+      }
+    });
+
+    await expect(
+      getAllProjects(event.queryStringParameters.isAll)
+    ).rejects.toThrow(errors.NotFoundError);
+
+    tracker.uninstall();
+  });
+
+  it('should return a list of projects with expected properties', async () => {
+    const event = {
+      queryStringParameters: {
+        isAll: 'true',
+      },
+    };
+
+    const result = await getAllProjects(event.queryStringParameters.isAll);
+
+    expect(result.statusCode).toBe(200);
+
+    expect(result.headers).toEqual({
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    });
+
+    const projects = JSON.parse(result.body);
+
+    expect(projects).toBeInstanceOf(Array);
+
+    expect(projects).toHaveLength(1);
+
+    const project = projects[0];
+    expect(project).toMatchObject({
+      project_id: expect.any(Number),
+      project_name: expect.any(String),
+      is_active: expect.any(Boolean),
+      created_by: expect.any(String),
+      last_updated_by: expect.any(String),
+      created_at: expect.any(String),
+      last_updated_at: expect.any(String),
+      project_code: expect.any(String),
+      material_budget: expect.any(String),
+      labor_budget: expect.any(String),
+      createdby: expect.any(String),
+      updatedby: expect.any(String),
+    });
   });
 });
