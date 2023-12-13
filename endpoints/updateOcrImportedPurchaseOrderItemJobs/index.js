ceedc7ef2ed63c65c1ace7f23ef3fe0c114b4420
Muhammad Navaid <navaidrasheed221@gmail.com>
2023-12-13 12:34:07 +0400

    ocr imported po item jobs bulk update

---
+++ endpoints/updateOcrImportedPurchaseOrderItemJobs/index.js
@@ -0,0 +1,24 @@
+import { createErrorResponse } from '/opt/nodej/apiResponseUtil.js';
+import updateOcrImportedPurchaseOrderItemJobs from './updateOcrImportedPurchaseOrderItemJobs.js';
+
+const handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body).ocrImportedPurchaseOrderDraftItems;
+    const ocrImporterPurchaseOrderDraftId =
+      event.pathParameters?.ocr_imported_purchase_order_draft_id;
+    const jobId = event.pathParameters?.job_id;
+    const userSub = event.requestContext.authorizer.sub;
+
+    return await updateOcrImportedPurchaseOrderItemJobs(
+      ocrImporterPurchaseOrderDraftId,
+      jobId,
+      body,
+      userSub
+    );
+  } catch (error) {
+    console.error('Error in handler: ', error.stack);
+    return createErrorResponse(error);
+  }
+};
+
+export { handler };
---
+++ endpoints/updateOcrImportedPurchaseOrderItemJobs/updateOcrImportedPurchaseOrderItemJobs.js
@@ -0,0 +1,83 @@
+import initializeKnex from '/opt/nodejs/db/index.js';
+import { createSuccessResponse } from '/opt/nodesjs/api/apiResponseUtil.js';
+import {
+  DatabaseError,
+  BadRequestError,
+  InternalServerError,
+} from '/opt/nodejs/errors.js';
+
+let knexInstance;
+
+const initializeDb = async () => {
+  try {
+    if (!knexInstance) {
+      knexInstance = await initializeKnex();
+    }
+  } catch (error) {
+    console.error('Error initializing database:', error.stack);
+    throw new DatabaseError('failed to initialize the database.');
+  }
+};
+
+const updateOcrImportedPurchaseOrderItemJobs = async (
+  ocrImportedPurchaseOrderId,
+  jobId,
+  ocrImportedPurchaseOrderDraft,
+  userSub
+) => {
+  await initializeDb();
+
+  if (!Array.isArray(ocrImportedPurchaseOrderDraft)) {
+    throw new BadRequestError(
+      'The ocrImportedPurchaseOrderDraft must be an array'
+    );
+  }
+  if (!ocrImportedPurchaseOrderId) {
+    throw new BadRequestError(
+      'Missing ocr_imported_purchase_order_id path parameter'
+    );
+  }
+  if (!jobId) {
+    throw new BadRequestError('Missing job_id path parameter');
+  }
+
+  try {
+    const user = await knexInstance('user')
+      .where('cognito_sub', userSub)
+      .pluck('user_id');
+
+    const ocrImportedPurchaseOrderDraftItemIds =
+      ocrImportedPurchaseOrderDraft.ocrImportedPurchaseOrderDraftItems.map(
+        (item) => item.ocr_imported_purchase_order_draft_item_id
+      );
+
+    const updatedOcrImportedPODraftItemJobs = await knexInstance(
+      'ocr_imported_purchase_order_draft_item'
+    )
+      .update({
+        project_id: jobId,
+        last_updated_at: knexInstance.raw('NOW()'),
+        last_updated_by: user[0],
+      })
+      .where('ocr_imported_purchase_order_draft_id', ocrImportedPurchaseOrderId)
+      .whereIn(
+        'ocr_imported_purchase_order_draft_item_id',
+        ocrImportedPurchaseOrderDraftItemIds
+      )
+      .returning('*');
+
+    return createSuccessResponse({
+      message:
+        'Ocr Imported Purcahse Order Draft Item Job Updated Successfully',
+      data: updatedOcrImportedPODraftItemJobs,
+    });
+  } catch (error) {
+    console.error(
+      'Error in updateOcrImportedPurchaseOrderItemJobs',
+      error.stack
+    );
+    throw new InternalServerError();
+  }
+};
+
+export default updateOcrImportedPurchaseOrderItemJobs;
