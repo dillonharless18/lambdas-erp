dfe62c72aa3885bf0ce50e19ee945cbe5385e015
Shakir Ullah <shakir@cloudfruit.com>
2023-12-22 20:40:25 +0500

    updated

--- endpoints/deleteOcrImportedPurchaseOrderItems/index.js
+++ endpoints/deleteOcrImportedPurchaseOrderItems/index.js
@@ -1,14 +1,16 @@
-import { createErrorResponse } from '/opt/nodejs/apiResponseUtil.js';
-import deleteOcrImportedPurchaseOrderItems from './deleteOcrImportedPurchaseOrderItems.js';
+import { createErrorResponse } from "/opt/nodejs/apiResponseUtil.js";
+import deleteOcrImportedPurchaseOrderItems from "./deleteOcrImportedPurchaseOrderItems.js";
 
 const handler = async (event) => {
-  try {
-    const body = JSON.parse(event.body);
-    const userSub = event.requestContext.authorizer.sub;
+    try {
+        const body = JSON.parse(event.body);
+        const userSub = event.requestContext.authorizer.sub;
 
-    return await deleteOcrImportedPurchaseOrderItems(body, userSub);
-  } catch (error) {
-    console.error('Error in handler:', error.stack);
-    return createErrorResponse(error);
-  }
+        return await deleteOcrImportedPurchaseOrderItems(body, userSub);
+    } catch (error) {
+        console.error("Error in handler:", error.stack);
+        return createErrorResponse(error);
+    }
 };
+
+export { handler };
