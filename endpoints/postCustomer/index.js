8f41062914a15fc755fad4a89391c7adfd888cb1
Muhammad Navaid <navaidrasheed221@gmail.com>
2024-01-01 22:12:33 +0400

    postCustomer endpoint

---
+++ endpoints/postCustomer/index.js
@@ -0,0 +1,14 @@
+import postCustomer from './postCustomer.js';
+import { createErrorResponse } from '/opt/nodejs/apiResponseUtil.js';
+
+const handler = async (event) => {
+  try {
+    const body = JSON.parse(event.body).customerData;
+    const userSub = event.requestContext.authorizer.sub;
+
+    return await postCustomer(body, userSub);
+  } catch (error) {
+    console.error(error.stack);
+    return createErrorResponse(error);
+  }
+};
