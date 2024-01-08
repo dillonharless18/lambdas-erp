7168a00f446d7943d0e0a1da46cdc9aea181c5cc
Shakir Ullah <shakir@cloudfruit.com>
2024-01-08 22:10:15 +0500

    onexerp 218

--- endpoints/getAllUsers/index.js
+++ endpoints/getAllUsers/index.js
@@ -1,16 +1,19 @@
-import getAllUsers from './getAllUsers.js';
-import { createErrorResponse } from '/opt/nodejs/apiResponseUtil.js';
+import getAllUsers from "./getAllUsers.js";
+import { createErrorResponse } from "/opt/nodejs/apiResponseUtil.js";
 
 const handler = async (event, context) => {
-  try {
-    const queryParams = event.queryStringParameters;
-    const userRole = queryParams ? queryParams.role : null;
+    try {
+        const queryParams = event.queryStringParameters;
+        const userRole = queryParams ? queryParams.role : null;
+        const searchText = queryParams?.searchText;
+        const pageNumber = queryParams?.pageNumber;
+        const pageSize = queryParams?.pageSize;
 
-    return await getAllUsers(userRole);
-  } catch (error) {
-    console.error('Error in handler:', error);
-    return createErrorResponse(error);
-  }
+        return await getAllUsers(userRole, searchText, pageNumber, pageSize);
+    } catch (error) {
+        console.error("Error in handler:", error);
+        return createErrorResponse(error);
+    }
 };
 
 export { handler };
