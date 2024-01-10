b1409808bbc79a5e0d53f95e3adbfeb93addbf00
Shakir Ullah <shakir@cloudfruit.com>
2024-01-10 21:10:49 +0500

    updated test case

--- endpoints/getAllProjects/getAllProjects.test.js
+++ endpoints/getAllProjects/getAllProjects.test.js
@@ -1,122 +1,168 @@
-const mockDb = require('mock-knex');
+const mockDb = require("mock-knex");
 
 jest.mock(
-  '/opt/nodejs/db/index.js',
-  () => ({
-    __esModule: true,
-    default: () => {
-      const knex = require('knex')({
-        client: 'pg',
-      });
-      mockDb.mock(knex);
-      return knex;
-    },
-  }),
-  { virtual: true }
-);
-
-jest.mock(
-  '/opt/nodejs/errors.js',
-  () => require('../../__mocks__/errosMock.js'),
-  {
-    virtual: true,
-  }
-);
-jest.mock(
-  '/opt/nodejs/apiResponseUtil.js',
-  () => require('../../__mocks__/apiResponseUtilMock.js'),
-  { virtual: true }
-);
-
-describe('getAllProjects', () => {
-  let getAllProjects;
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-
-    getAllProjects = require('./getAllProjects').default;
-
-    const tracker = mockDb.getTracker();
-    tracker.install();
-    tracker.on('query', (query) => {
-      query.response([
-        {
-          project_id: 1,
-          project_name: 'Job 1',
-          is_active: true,
-          created_by: '1b3ef41c-23af-4eee-bbd7-5610b38e37f2',
-          last_updated_by: '4566a3j7-92a8-40f8-8f00-f8fc355bbk7g',
-          created_at: '2023-05-17T03:27:13.141Z',
-          last_updated_at: '2023-08-16T17:56:42.522Z',
-          project_code: 'P1',
-          material_budget: '24.00',
-          labor_budget: '12.00',
-          createdby: 'Default Default',
-          updatedby: 'Shakir Ullah',
+    "/opt/nodejs/db/index.js",
+    () => ({
+        __esModule: true,
+        default: () => {
+            const knex = require("knex")({
+                client: "pg",
+            });
+            mockDb.mock(knex);
+            return knex;
         },
-      ]);
+    }),
+    { virtual: true }
+);
+
+jest.mock(
+    "/opt/nodejs/errors.js",
+    () => require("../../__mocks__/errosMock.js"),
+    {
+        virtual: true,
+    }
+);
+jest.mock(
+    "/opt/nodejs/apiResponseUtil.js",
+    () => require("../../__mocks__/apiResponseUtilMock.js"),
+    { virtual: true }
+);
+
+describe("getAllProjects", () => {
+    let getAllProjects;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        getAllProjects = require("./getAllProjects").default;
+
+        const tracker = mockDb.getTracker();
+        tracker.install();
+        tracker.on("query", (query) => {
+            query.response([
+                {
+                    project_id: 1,
+                    project_name: "Job 1",
+                    is_active: true,
+                    created_by: "1b3ef41c-23af-4eee-bbd7-5610b38e37f2",
+                    last_updated_by: "4566a3j7-92a8-40f8-8f00-f8fc355bbk7g",
+                    created_at: "2023-05-17T03:27:13.141Z",
+                    last_updated_at: "2023-08-16T17:56:42.522Z",
+                    project_code: "P1",
+                    material_budget: "24.00",
+                    labor_budget: "12.00",
+                    createdby: "Default Default",
+                    updatedby: "Shakir Ullah",
+                },
+            ]);
+        });
     });
-  });
 
-  afterEach(() => {
-    mockDb.getTracker().uninstall();
-  });
-
-  it('should return a list of projects', async () => {
-    const event = {
-      queryStringParameters: {
-        isAll: 'true',
-      },
-    };
-
-    const result = await getAllProjects(event.queryStringParameters.isAll);
-
-    expect(result.statusCode).toBe(200);
-    expect(result.headers).toEqual({
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
+    afterEach(() => {
+        mockDb.getTracker().uninstall();
     });
 
-    const body = JSON.parse(result.body);
-    expect(body).toBeInstanceOf(Array);
-  });
+    it("should return a list of projects", async () => {
+        const {
+            queryStringParameters: {
+                isAll,
+                searchText,
+                pageNumber,
+                pageSize,
+                status,
+            },
+        } = {
+            queryStringParameters: {
+                isAll: "true",
+                searchText: "",
+                pageNumber: 1,
+                pageSize: 10,
+                status: "true",
+            },
+        };
 
-  it('should return a list of projects with expected properties', async () => {
-    const event = {
-      queryStringParameters: {
-        isAll: 'true',
-      },
-    };
+        const result = await getAllProjects(
+            isAll,
+            searchText,
+            pageNumber,
+            pageSize,
+            status
+        );
 
-    const result = await getAllProjects(event.queryStringParameters.isAll);
+        expect(result.statusCode).toBe(200);
+        expect(result.headers).toEqual({
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        });
 
-    expect(result.statusCode).toBe(200);
-
-    expect(result.headers).toEqual({
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
+        const body = JSON.parse(result.body);
+        if (Array.isArray(body)) {
+            expect(body).toBeInstanceOf(Array);
+        } else if (
+            typeof body === "object" &&
+            body !== null &&
+            "data" in body &&
+            "totalCount" in body
+        ) {
+            expect(body).toEqual(
+                expect.objectContaining({
+                    data: expect.any(Array),
+                    totalCount: expect.any(String),
+                })
+            );
+        }
     });
 
-    const projects = JSON.parse(result.body);
+    it("should return a list of projects with expected properties", async () => {
+        const event = {
+            queryStringParameters: {
+                isAll: "true",
+            },
+        };
 
-    expect(projects).toBeInstanceOf(Array);
+        const result = await getAllProjects(event.queryStringParameters.isAll);
 
-    expect(projects).toHaveLength(1);
+        expect(result.statusCode).toBe(200);
 
-    const project = projects[0];
-    expect(project).toMatchObject({
-      project_id: expect.any(Number),
-      project_name: expect.any(String),
-      is_active: expect.any(Boolean),
-      created_by: expect.any(String),
-      last_updated_by: expect.any(String),
-      created_at: expect.any(String),
-      last_updated_at: expect.any(String),
-      project_code: expect.any(String),
-      material_budget: expect.any(String),
-      labor_budget: expect.any(String),
-      createdby: expect.any(String),
-      updatedby: expect.any(String),
+        expect(result.headers).toEqual({
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+        });
+
+        const projects = JSON.parse(result.body);
+        let project = [];
+        if (Array.isArray(projects)) {
+            expect(projects).toBeInstanceOf(Array);
+            project = projects[0];
+        } else if (
+            typeof projects === "object" &&
+            projects !== null &&
+            "data" in projects &&
+            "totalCount" in projects
+        ) {
+            expect(projects).toEqual(
+                expect.objectContaining({
+                    data: expect.any(Array),
+                    totalCount: expect.any(String),
+                })
+            );
+
+            project = projects.data[0];
+        }
+
+        expect(project).toMatchObject({
+            project_id: expect.any(Number),
+            project_name: expect.any(String),
+            is_active: expect.any(Boolean),
+            created_by: expect.any(String),
+            last_updated_by: expect.any(String),
+            created_at: expect.any(String),
+            last_updated_at: expect.any(String),
+            project_code: expect.any(String),
+            material_budget: expect.any(String),
+            labor_budget: expect.any(String),
+            createdby: expect.any(String),
+            updatedby: expect.any(String),
+        });
     });
-  });
 });
