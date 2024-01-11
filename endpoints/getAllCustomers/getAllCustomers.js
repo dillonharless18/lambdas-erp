4f6558aa399152a7f5bbed527ff34e0f6ced4c58
Muhammad Navaid <navaidrasheed221@gmail.com>
2024-01-11 14:52:38 +0400

    update getAllcustomer included customerContacts

--- endpoints/getAllCustomers/getAllCustomers.js
+++ endpoints/getAllCustomers/getAllCustomers.js
@@ -18,29 +18,40 @@ const initializeDb = async () => {
 const getAllCustomers = async (isAll) => {
   await initializeDb();
   try {
-    let query = knexInstance
-    .select(
-      "c.*",
-      knexInstance.raw(
-        '("createdBy".first_name || \' \' || "createdBy".last_name) AS CreatedBy'
-      ),
-      knexInstance.raw(
-        '("updatedBy".first_name || \' \' || "updatedBy".last_name) AS UpdatedBy'
+    let query = await knexInstance
+      .select(
+        'c.*',
+        knexInstance.raw(
+          '("createdBy".first_name || \' \' || "createdBy".last_name) AS CreatedBy'
+        ),
+        knexInstance.raw(
+          '("updatedBy".first_name || \' \' || "updatedBy".last_name) AS UpdatedBy'
+        ),
+        knexInstance.raw('json_agg(cc.*) as customerContacts')
       )
-    )
-    .from("customer as c")
-    .orderBy("c.created_at", "asc")
-    .join("user as createdBy", "createdBy.user_id", "=", "c.created_by")
-    .join("user as updatedBy", "updatedBy.user_id", "=", "c.last_updated_by");
+      .from('customer as c')
+      .orderBy('c.created_at', 'asc')
+      .join('user as createdBy', 'createdBy.user_id', '=', 'c.created_by')
+      .join('user as updatedBy', 'updatedBy.user_id', '=', 'c.last_updated_by')
+      .leftJoin(
+        'customer_contact as cc',
+        'c.customer_id',
+        '=',
+        'cc.customer_id'
+      )
+      .groupBy(
+        'c.customer_id',
+        'createdBy.first_name',
+        'updatedBy.first_name',
+        'createdBy.last_name',
+        'updatedBy.last_name'
+      );
 
     if (!isAll) {
-        query = query.where('c.is_active', true);
-      }
+      query = query.where('c.is_active', true);
+    }
 
     const customers = await query;
-    if (!customers || customers.length === 0) {
-      throw new NotFoundError('No customers found.');
-    }
 
     return createSuccessResponse(customers);
   } catch (error) {
